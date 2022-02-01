bfd3ec96bd8282fba33b1bfd81314d3ba66f76f8
unknown <faezehjafarpour75@gmail.com>
2022-02-01 22:07:32 +0330

    taghelper

---
+++ ExCreatTagHelper/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExCreatTagHelper.TagHelpers
+{
+    //ححتما باید  ارث برای داشته باشه
+
+    //[HtmlTargetElement("tag-name")]
+    public class EmailTagHelper :TagHelper
+    {
+        private string Domain = "gmail.com";
+        private string MailTo { get; set; }
+        
+        public override void Process(TagHelperContext tagHelperContext , TagHelperOutput tagHelperOutput)
+        {
+            tagHelperOutput.TagName = "a";
+            string adress = MailTo + "@" + Domain;
+            tagHelperOutput.Attributes.SetAttribute("href", "mailto:" + adress);
+            tagHelperOutput.Content.SetContent(adress);
+        }
+    }
+}
